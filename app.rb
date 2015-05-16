a2a1093c61c24cbd5b97aa278b5e09a35070ceb0
Phil Esterman <philesterman@MacBook-Pro.local>
2015-05-16 09:35:45 -0400

    signup configured

--- app.rb
+++ app.rb
@@ -15,25 +15,29 @@ get '/sms' do
 	@user = User.find_by_phone(params[:From]) 
 
 	if @user == nil #new user, add her
-		@user = User.create(name: "empty", phone: params[:From])
+		@user = User.create(name: "empty", child_birthdate: "empty", time: "empty", phone: params[:From])
   		twiml = Twilio::TwiML::Response.new do |r|
-   			r.Message "Thanks ya jew- You've got StoryTime JSC!"
+   			r.Message "StoryTime: Thanks for signing up! Reply with your child's birthdate in MMYY format (e.g. 0912 for September 2013)."
     	end
     	twiml.text
-    elsif @user.name.eql? "empty" #update name
-    	@user.name = params[:Body]
+    elsif @user.child_birthdate.eql? "empty" #update child's birthdate
+    	@user.child_birthdate = params[:Body]
     	@user.save
-    end 
-
-
-
-
-  @user = User.create(name: "empty", phone: params[:From])
-  twiml = Twilio::TwiML::Response.new do |r|
-    r.Message "Thanks ya jew- You've got StoryTime JSC!"
-  end
-  twiml.text
-end
+  		twiml = Twilio::TwiML::Response.new do |r|
+   			r.Message "StoryTime: Great! You've got free nightly stories by text. When do you want them? Reply with your preferred time and your child's name (e.g. 5:30pm Brianna)"
+		end
+ 		twiml.text
+ 	elsif @user.time.eql? "empty" #update time 
+ 		response = params[:Body]
+ 		arr = response.split
+ 		@user.time = arr[0]
+ 		@user.name = arr[1]
+    	@user.save
+  		twiml = Twilio::TwiML::Response.new do |r|
+   			r.Message "StoryTime: Sounds good! We'll send you and #{arr[1]} a new story each night at #{arr[0]}."
+		end
+ 		twiml.text
+	end
 
 
 
