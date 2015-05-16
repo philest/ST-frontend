935c70f6b4cab34197b4d920e2249ca8432d7a35
Phil Esterman <philesterman@MacBook-Pro.local>
2015-05-15 21:05:49 -0400

    local db migration worked! both pg; created users table

--- Gemfile
+++ Gemfile
@@ -1,3 +1,13 @@
 source "https://rubygems.org"
 
 gem "sinatra"
+gem "activerecord"
+gem "sinatra-activerecord"
+
+group :development do
+ gem 'sqlite3'
+end
+
+group :production do
+ gem 'pg'
+end
--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+require './config/environments' #database configuration
+require './models/user' #add the user model
 
 get '/' do
 	erb :main
 end
 
+# post '/users/new/:name' do
