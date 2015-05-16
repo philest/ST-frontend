3b8003839eb1ee431058e5c9a070a9e7d8f7c6a0
Phil Esterman <philesterman@MacBook-Pro.local>
2015-05-16 12:04:37 -0400

    fixed split error

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ require './config/environments' #database configuration
 require './models/user' #add the user model
 require 'twilio-ruby'
 
-EMPTY_AGE = 999
-
+EMPTY_INT = 999
+EMPTY_STR = "empty"
 
 get '/' do
 	erb :main
@@ -18,21 +18,21 @@ get '/sms' do
 	@user = User.find_by_phone(params[:From]) 
 
 	if @user == nil #new user, add her
-		@user = User.create(child_name: "empty", child_age: EMPTY_AGE, time: "empty", phone: params[:From])
+		@user = User.create(child_name: EMPTY_STR, child_age: EMPTY_INT, time: EMPTY_STR, phone: params[:From])
   		twiml = Twilio::TwiML::Response.new do |r|
    			r.Message "StoryTime: Thanks for signing up! Reply with your child's age in years (e.g. 3)."
     	end
     	twiml.text
-    elsif @user.child_age == EMPTY_AGE #update child's birthdate
+    elsif @user.child_age == EMPTY_INT #update child's birthdate
     	@user.child_age = Integer(params[:Body])
     	@user.save
   		twiml = Twilio::TwiML::Response.new do |r|
    			r.Message "StoryTime: Great! You've got free nightly stories. Reply with your child's name and your preferred time to receive stories (e.g. Brianna 5:30pm)"
 		end
  		twiml.text
- 	elsif @user.time.eql? "empty" #update time and child's name
+ 	elsif @user.time.eql? EMPTY_STR #update time and child's name
  		response = params[:Body]
- 		arr = response.split
+ 		arr = response.split('+')
 
  		if arr.length == 2
 	 		#handle wrong order
@@ -53,6 +53,10 @@ get '/sms' do
 	 		end
 	 	else
 	 		raise "this format is incorrect. try again"
+	 		twiml = Twilio::TwiML::Response.new do |r|
+   				r.Message "Something broke-- ERROR SPLIT."
+			end
+ 			twiml.text
 	 	end
 
     	@user.save
