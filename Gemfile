2d3f2bea4aff91982b232368f57454d2f2a0168b
Phil Esterman <philesterman@MacBook-Pro.local>
2015-05-15 22:59:11 -0400

    fixed sms

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
 gem "sinatra"
+gem "rubygems"
 gem "activerecord"
 gem "sinatra-activerecord"
 gem "twilio-ruby"
--- app.rb
+++ app.rb
@@ -9,14 +9,24 @@ get '/' do
 end
 
 # register an incoming SMS
-post '/' do
-	#add user to db
-	@user = User.create(name: "empty", phone: params[:From])
-	twiml = Twilio::TwiML::Response.new do |r|
-    	r.Message "Thanks for signing up for StoryTime!"
-  	end
-  	twiml.text
+get '/sms' do
+  @user = User.create(name: "empty", phone: params[:From])
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message "Hey Monkey. Thanks for the message!"
+  end
+  twiml.text
 end
+
+
+
+
+	#add user to db
+# 	@user = User.create(name: "empty", phone: params[:From])
+# 	twiml = Twilio::TwiML::Response.new do |r|
+#     	r.Message "Thanks for signing up for StoryTime!"
+#   	end
+#   	twiml.text
+# end
 
 get '/users/new/:phone' do
 	# add user to db
