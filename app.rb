1d30550f13d0b2af3d61dcf91d6670e8f51e67d4
Phil Esterman <philesterman@MacBook-Pro.local>
2015-05-16 08:45:35 -0400

    added birthdate, time migrations; configured devt db

--- app.rb
+++ app.rb
@@ -10,6 +10,24 @@ end
 
 # register an incoming SMS
 get '/sms' do
+	#check if new user
+	#returns nil if not found
+	@user = User.find_by_phone(params[:From]) 
+
+	if @user == nil #new user, add her
+		@user = User.create(name: "empty", phone: params[:From])
+  		twiml = Twilio::TwiML::Response.new do |r|
+   			r.Message "Thanks ya jew- You've got StoryTime JSC!"
+    	end
+    	twiml.text
+    elsif @user.name.eql? "empty" #update name
+    	@user.name = params[:Body]
+    	@user.save
+    end 
+
+
+
+
   @user = User.create(name: "empty", phone: params[:From])
   twiml = Twilio::TwiML::Response.new do |r|
     r.Message "Thanks ya jew- You've got StoryTime JSC!"
