749b753d6b532fe8434a74383dcf54f881eddf6e
Phil Esterman <philesterman@MacBook-Pro.local>
2015-05-17 22:02:53 -0400

    not working

--- workers/some_worker.rb
+++ workers/some_worker.rb
@@ -13,7 +13,7 @@ class SomeWorker
   									32, 34, 36, 38, 40, 42, 44, 46, 48, 50,
   									52, 54, 56, 58) } #set explicitly because of ice-cube sluggishness
 
-  def self.perform(*args)
+  def perform(*args)
     puts "doing hard work!!"
   end
 
