d019657157356e586aa5d5f3a0114d0deee85c07
Phil Esterman <philesterman@MacBook-Pro.local>
2015-05-20 10:20:12 -0400

    workers cleaned

--- workers/some_worker.rb
+++ workers/some_worker.rb
@@ -7,16 +7,16 @@ require '../models/user'           #add the user model
 
 
 class SomeWorker
- #  include Sidekiq::Worker
- #  include Sidetiq::Schedulable
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
 	
-	# sidekiq_options retry: false #if fails, don't resent (multiple texts)
+	sidekiq_options retry: false #if fails, don't resent (multiple texts)
 
 
-  # recurrence { hourly.minute_of_hour(0, 2, 4, 6, 8, 10,
-  # 									12, 14, 16, 18, 20, 22, 24, 26, 28, 30,
-  # 									32, 34, 36, 38, 40, 42, 44, 46, 48, 50,
-  # 									52, 54, 56, 58) } #set explicitly because of ice-cube sluggishness
+  recurrence { hourly.minute_of_hour(0, 2, 4, 6, 8, 10,
+  									12, 14, 16, 18, 20, 22, 24, 26, 28, 30,
+  									32, 34, 36, 38, 40, 42, 44, 46, 48, 50,
+  									52, 54, 56, 58) } #set explicitly because of ice-cube sluggishness
 
   def perform(*args)
     
@@ -141,13 +141,15 @@ class SomeWorker
 
 
 
-
-
-
-
-
-  
-
-
-  
 end
+end
+
+
+
+
+
+  
+
+
+  
+
