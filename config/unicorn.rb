96a44c72b6c28a982788ea57c5af04942c74a72a
Phil Esterman <philesterman@MacBook-Pro.local>
2015-05-22 22:11:16 -0400

    trying unicorn worker processes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,33 @@
 
 worker_processes 3
 timeout 30
+
+app_path = File.expand_path(File.dirname(__FILE__) + '/..')
+
+listen app_path + '/tmp/unicorn.sock', backlog: 64
+
+before_fork do |server, worker|
+   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 5 -v -r ./app.rb")
+end
+
+# If using ActiveRecord, disconnect (from the database) before forking.
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+end
+
+# After forking, restore your ActiveRecord connection.
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.establish_connection
+end
+
+
+working_directory app_path
+
+pid app_path + '/tmp/unicorn.pid'
+
+listen(3000, backlog: 64) if ENV['RACK_ENV'] == 'development'
+
+stderr_path app_path + '/log/unicorn.log'
+stdout_path app_path + '/log/unicorn.log'
