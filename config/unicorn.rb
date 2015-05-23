4903eab996ea7fbb3ceaf021132d523427cf4394
Phil Esterman <philesterman@MacBook-Pro.local>
2015-05-22 22:32:04 -0400

    uni config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,6 +5,9 @@ timeout 30
 app_path = File.expand_path(File.dirname(__FILE__) + '/..')
 
 listen app_path + '/tmp/unicorn.sock', backlog: 64
+listen 8080, :tcp_nopush => true
+
+
 
 before_fork do |server, worker|
    @sidekiq_pid ||= spawn("bundle exec sidekiq -c 5 -v -r ./app.rb")
@@ -29,6 +32,6 @@ pid app_path + '/tmp/pids/unicorn.pid'
 
 listen(3000, backlog: 64) if ENV['RACK_ENV'] == 'development'
 
-stderr_path app_path + '/log/unicorn.stderr.log'
-stdout_path app_path + '/log/unicorn.stdout.log'
+stderr_path app_path + '/log/unicorn.log'
+stdout_path app_path + '/log/unicorn.log'
 
