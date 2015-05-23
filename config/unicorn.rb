27a83e7d956fa717defdfd9c8b47dad76241a168
Phil Esterman <philesterman@MacBook-Pro.local>
2015-05-22 22:58:44 -0400

    stderr

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,16 +2,21 @@
 worker_processes 3
 timeout 30
 
+
 app_path = File.expand_path(File.dirname(__FILE__) + '/..')
 
+
+working_directory app_path
 
 
 stderr_path app_path + '/log/unicorn.stderr.log'
 stdout_path app_path + '/log/unicorn.stdout.log'
 
+# stderr_path app_path + '/log/unicorn.stderr.log'
+# stdout_path app_path + '/log/unicorn.stdout.log'
+
 
 listen app_path + '/tmp/unicorn.sock', backlog: 64
-listen 8080, :tcp_nopush => true
 
 
 
@@ -19,25 +24,20 @@ before_fork do |server, worker|
    @sidekiq_pid ||= spawn("bundle exec sidekiq -c 5 -v -r ./app.rb")
 end
 
-# If using ActiveRecord, disconnect (from the database) before forking.
-before_fork do |server, worker|
-  defined?(ActiveRecord::Base) &&
-    ActiveRecord::Base.connection.disconnect!
-end
+# # If using ActiveRecord, disconnect (from the database) before forking.
+# before_fork do |server, worker|
+#   defined?(ActiveRecord::Base) &&
+#     ActiveRecord::Base.connection.disconnect!
+# end
 
-# After forking, restore your ActiveRecord connection.
-after_fork do |server, worker|
-  defined?(ActiveRecord::Base) &&
-    ActiveRecord::Base.establish_connection
-end
+# # After forking, restore your ActiveRecord connection.
+# after_fork do |server, worker|
+#   defined?(ActiveRecord::Base) &&
+#     ActiveRecord::Base.establish_connection
+# end
 
 
-working_directory app_path
 
 pid app_path + '/tmp/pids/unicorn.pid'
 
 listen(3000, backlog: 64) if ENV['RACK_ENV'] == 'development'
-
-stderr_path app_path + '/log/unicorn.stderr.log'
-stdout_path app_path + '/log/unicorn.stdout.log'
-
