c62fa48dc415c5d04a0023fef8a2d9cca66c470a
Phil Esterman <philesterman@MacBook-Pro.local>
2015-05-28 14:09:01 -0400

    story_number migration; @storyArr initialize

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150523201445) do
+ActiveRecord::Schema.define(version: 20150528180137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150523201445) do
     t.integer  "child_age"
     t.string   "child_name"
     t.string   "carrier"
+    t.integer  "story_number",    default: 0
   end
 
 end
--- workers/some_worker.rb
+++ workers/some_worker.rb
@@ -20,99 +20,29 @@ class SomeWorker
   									32, 34, 36, 38, 40, 42, 44, 46, 48, 50,
   									52, 54, 56, 58) } #set explicitly because of ice-cube sluggishness
 
+
+
+  #create the structure for holding all stories (with sub MMS and age-approp SMS)
+  @@storyArr = Array.new #holds all the story structs!
+
+  def self.buildStoryArr
+    Struct.new("Story", :mmsArr, :smsHash) #creates a new type of struct for Story
+
+      #Day 0:
+
+        #create the mmsArr for the story:
+        mmsArr = ["http://i.imgur.com/GAyZhKa.jpg"]
+
+        #create the sms Hash.
+        smsHash = SomeWorker.makeHash("test3", "test4", "test5")
+
+        #zeroth
+        zero = Struct::Story.new(mmsArr, smsHash)
+        @@storyArr.push zero 
+  end
+
+
   def perform(*args)
-    
-  	@story = [ "StoryTime: Enjoy tonight's story:
-
-  Now We Are Six
-
-  When I was one,
-  I had just begun.
-
-  When I was two,
-  I was nearly new.
-
-  When I was three,
-  I was hardly me.
-
-  When I was four,
-  I was not much more.
-
-  When I was five,
-  I was just alive.
-
-  But now I am six,
-  I'm as clever as clever.
-  
-  So I think I'll be six
-  Now and forever.
-
-  a)Ask your child how old they are and what their birthday is?
-
-  b)Does your child think it’s possible to be 6 years old forever?
-  
-  c)What is the smallest number your child knows? The biggest? Can your child count all the way to it?",
-
-  "StoryTime: Keep up the great work!
-
-  Unscratchable Itch
-
-  There is a spot that you can’t scratch
-  Right between your shoulder blades,
-  Like an egg that just won’t hatch
-  Here you set and there it stay
-
-  Turn and squirm and try to reach it,
-  Twist your neck and bend your back,
-
-  Hear your elbows creak and creak,
-  Stretch your fingers, now you bet it’s
-  Going to reach –
-  
-  no that won’t get it-
-  Hold your breath and stretch and pray,
-  Only just an inch away,
-
-  Worse than a sunbeam you can’t catch
-  Is that one spot that
-  You can’t scratch.
-
-a)Is there a spot on your back that you can’t reach? Try the other hand! Can you reach your toes…without bending your knees!
-
-b)Reread the first line of the poem. But this time, clap your hands as you say each syllable.
-Have your child repeat the line. Help them clap their hands as they say each syllable.
-
-c)Try this for the second line! The poet rhymes the word \"scratch\" with \"hatch.\" Can you think of any other words that rhyme with \"scratch?\" See if you can name 5!",
-"StoryTime: Here's tonight's poem:
-
-  Where the Sidewalk Ends
-
-  There is a place where the sidewalk ends
-  And before the street begins.
-  And there the grass grows soft and white.
-  And there the sun burns crimson bright
-  And there the moon-bird rests from his flight
-  To cool in the peppermint wind.
-
-  Let us leave this place where the smoke blows black
-  And the dark street winds and bends.
-  Past the pits where the asphalt flowers grow
-  We shall walk with a walk that is measured and slow,
-  And watch where the chalk-white arrows go
-  To the place where the sidewalk ends.
-
-  Yes we'll walk with a walk that is measured and slow,
-  And we'll go where the chalk-white arrows go,
-  For the children, they mark, and the children,they know
-  The place where the sidewalk ends.
-
-a) Have you ever been to the end of the sidewalk? Would you want to go? What do you think it looks like there?
-
-b) Reread the third line. Ask your child to repeat it. Try to think of 10 other words that start with the letter “g”! 
-Can you make the shape of the letter “g” with your fingers? How about with your body?"]
-
-
-
 
 
     account_sid = ENV['TW_ACCOUNT_SID']
@@ -238,7 +168,11 @@ Can you make the shape of the letter “g” with your fingers? How about with y
 
 
 
-
+  #makes a hash to use in struct
+  def self.makeHash(three, four, five)
+    hash = { 3 => three, 4 => four, 5 => five}
+    return hash
+  end
 
   # helper methods
 
@@ -351,6 +285,11 @@ Can you make the shape of the letter “g” with your fingers? How about with y
     return cleanedTime
 
   end
+
+
+#start doing stuff!
+SomeWorker.buildStoryArr #Go!
+
 
 
 end
