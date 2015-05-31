c6951b489edc3f99ae33eb3af7f48c47f0c63d94
Phil Esterman <phil.esterman@yale.edu>
2015-05-31 15:41:31 -0400

    Set up TEST and DEVELOPMENT environments! along with their own DATABASES

--- config/environments.rb
+++ config/environments.rb
@@ -2,9 +2,7 @@
 #The environment variable DATABASE_URL should be in the following format:
 # => postgres://{user}:{password}@{host}:{port}/path
 
-
-
-configure :development, :production, :test do
+configure :development, :production do
  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://postgres:sharlach1@localhost/development')
 
 
@@ -20,3 +18,22 @@ ENV["REDISTOGO_URL"] = "redis://redistogo:120075187f5e39ba84e429f311eb69a5@hamme
 			:encoding => 'utf8'
 	)
 end
+
+#set up the test database
+configure :test do
+ db = URI.parse('postgres://postgres:sharlach1@localhost/test')
+
+
+#adding development REDIS config
+ENV["REDISTOGO_URL"] = "redis://redistogo:120075187f5e39ba84e429f311eb69a5@hammerjaw.redistogo.com:9787/"
+ 
+	ActiveRecord::Base.establish_connection(
+			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+			:host     => db.host,
+			:username => db.user,
+			:password => db.password,
+			:database => db.path[1..-1],
+			:encoding => 'utf8'
+	)
+end
+
