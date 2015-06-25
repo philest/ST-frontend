de19649d745bd0f7483cc8e698fbf20e4b44707a
Phil Esterman <phil.esterman@yale.edu>
2015-06-25 12:46:59 -0400

    post

--- app.rb
+++ app.rb
@@ -133,7 +133,7 @@ get '/mp3' do
 	send_file File.join(settings.public_folder, 'storytime_message.mp3')
 end
 
-get '/failed' do
+post '/failed' do
 	Helpers.smsRespondHelper("StoryTime: Hi! We're updating StoryTime now and are offline, but be sure to check back in the next day!")
 end
 
@@ -146,7 +146,7 @@ end
 
 
 # register an incoming SMS
-get '/sms' do
+post '/sms' do
 	workflow
 end
 
