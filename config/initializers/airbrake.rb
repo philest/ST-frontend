c7e46c85b4c225d6b3e5082697c192f4f9133b41
Phil Esterman <phil.esterman@yale.edu>
2016-06-02 13:04:28 -0400

    fixed navbar min-height

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |c|
-  c.project_id = ENV['AIRBRAKE_API_KEY']
-  c.project_key = ENV['AIRBRAKE_PROJECT_ID']
+  c.project_id = ENV['AIRBRAKE_PROJECT_ID']
+  c.project_key = ENV['AIRBRAKE_API_KEY']
 
   # Display debug output.
   c.logger.level = Logger::DEBUG
