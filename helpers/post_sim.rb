3f25158bf33fd4f7ffa2c68985fea4eb4b17bb39
jdmcpeek <david.mcpeek@yale.edu>
2016-06-21 14:01:08 -0400

    using quailtime

--- helpers/post_sim.rb
+++ helpers/post_sim.rb
@@ -10,14 +10,21 @@ Dotenv.load
 params = { :name_0 => "Phil",
 		   :phone_0 => "5612125831",
 		   'name_1' => "David",
-		   'phone_1' => "5612125888",
+		   'phone_1' => "8186897323",
 		   :name_2 => "Aub",
-		   :phone_2 => "5612125999",
+		   :phone_2 => "3013328953",
 		   "teacher_prefix" => "Ms.",
 		   "teacher_signature" => "Jones"
 		 }
 
+# params = {
+# 	:name_0 => "AssWipe", 
+# 	:phone_0 => "keyboard MADNESS!", 
+# 	:teacher_prefix => "Ms.",
+# 	:teacher_signature => "Jones"
+# }
+
 # binding.pry
 
 
-HTTParty.post(ENV['birdv_url'], body: params)
+HTTParty.post(ENV['quailtime_url'], body: params)
