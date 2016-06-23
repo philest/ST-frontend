b24eb0af11b5a0633303db3db18c251c75c40e78
Phil Esterman <phil.esterman@yale.edu>
2016-06-23 11:57:16 -0400

    basic specs for Teacher Interface functionality

--- spec/views/enroll_spec.rb
+++ spec/views/enroll_spec.rb
@@ -44,7 +44,66 @@ describe 'Teacher Enrollment Interface', :type => :feature, :js => :true do
       visit '/enroll'
       expect(page).to_not have_content "fake fake fake... / 
                                         just my analysis."
+  end
+
+  context "when properly enrolling a family", :type => :feature, :js => :true do
+    
+    before(:each) do
+      visit '/enroll'   
+      select('Ms.', :from => 'teacher_prefix')
+      fill_in('teacher_signature', :with => 'Stobierski')
+      fill_in('name_0', :with => 'Art Vandelay')
+      fill_in('phone_0', :with => '5612125888')
+    end
+
+    it "doesn't give invalid phone errors" do
+      expect(page).to_not have_content "Invalid"
+    end
+
+    it "does redirect" do
+      expect(page).to have_content "Great!"
+    end
+
   end 
+
+  context "when entering invalid phone", :type => :feature, :js => :true do
+    
+    before(:each) do
+      visit '/enroll'   
+      select('Ms.', :from => 'teacher_prefix')
+      fill_in('teacher_signature', :with => 'Stobierski')
+      fill_in('name_0', :with => 'Art Vandelay')
+      fill_in('phone_0', :with => '5612125')
+      fill_in('name_1', :with => 'Phil Vandelay')
+      fill_in('phone_0', :with => 'fakefake')
+
+    end
+
+    it "gives invalid phone errors" do
+      expect(page).to have_content "Invalid"
+    end
+
+  end 
+
+  context "when submitting with no name", :type => :feature, :js => :true do
+    
+    before(:each) do
+      visit '/enroll'   
+      fill_in('name_0', :with => 'Art Vandelay')
+      fill_in('phone_0', :with => '5612125888')
+      click_button('Add people')
+    end
+
+    it "shows error message" do
+      expect(page).to have_content "Please correct"
+    end
+
+    it "does not redirect" do
+      expect(page).to have_content "get your class free stories"
+    end
+
+  end 
+
 
 
 end
