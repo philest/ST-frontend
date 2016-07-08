5a9a55e122302bc5cbf83e6d73fa8f75395ce903
Phil Esterman <phil.esterman@yale.edu>
2016-07-08 14:38:12 -0400

    better models dir

---
+++ models/variation.rb
@@ -0,0 +1,14 @@
+#  variation.rb 	                          Phil Esterman		
+# 
+#  A variation for an A/B experiment: one particular
+#  treatment. Bridges experiment and users. 
+#
+#  E.g. for an experiment on when to send messages, a
+#  variation could be 7:00pm. 
+#  --------------------------------------------------------
+
+class Variation < ActiveRecord::Base
+	has_many :users
+	belongs_to :experiment
+	belongs_to :user
+end
