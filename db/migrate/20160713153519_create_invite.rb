368fa7fdb950766fc60204560781206e66e2afdd
Phil Esterman <phil.esterman@yale.edu>
2016-07-13 11:42:57 -0400

    added invite table

---
+++ db/migrate/20160713153519_create_invite.rb
@@ -0,0 +1,9 @@
+class CreateInvite < ActiveRecord::Migration
+  def change
+
+  	 create_table :invites do |t|
+  		t.string :email
+  	 end
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303211046) do
+ActiveRecord::Schema.define(version: 20160713153519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 20160303211046) do
 
   create_table "followers", force: :cascade do |t|
     t.string "name"
+    t.string "email"
+  end
+
+  create_table "invites", force: :cascade do |t|
     t.string "email"
   end
 
