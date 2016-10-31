584abf43a52ef01172e86cd31c1d4602602dc42c
jdmcpeek <david.mcpeek@yale.edu>
2016-10-31 13:24:04 -0400

    adjust signup post to st-enroll

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -5,12 +5,14 @@ if ENV['RACK_ENV'] != 'production'
   Dotenv.load
 end
 
+Aws.eager_autoload!
+
 Aws.config.update({
   region: 'us-east-1',
   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
 })
 
-S3 = Aws::S3::Resource.new
+S3 = Aws::S3::Resource.new(region: 'us-east-1')
 
 # TEACHER_MATERIALS = S3.bucket('teacher-flyers')
 # puts bucket.exists?
