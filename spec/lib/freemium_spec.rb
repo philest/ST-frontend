bb4caebba347751584b758d37c1f8d90e3b5182e
jdmcpeek <david.mcpeek@yale.edu>
2017-01-25 17:29:50 -0500

    first draft - still need to write specs

---
+++ spec/lib/freemium_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+require 'app'
+require 'timecop'
+require 'active_support/time'
+require 'workers'
+
+
+
+describe 'freemium signup' do
+
+
+  context 'parents' do
+
+
+  end
+
+
+  context 'teachers' do
+
+
+  end
+
+
+  context 'admins' do
+
+
+  end
+
+
+
+  
+end
