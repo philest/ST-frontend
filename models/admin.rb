4fbfc979ffc08ff729fa9880ee2db3e808ebd8d2
jdmcpeek <david.mcpeek@yale.edu>
2017-02-23 14:02:27 -0500

    email-phone mostly finished, quicklinks working

--- models/admin.rb
+++ models/admin.rb
@@ -1,7 +1,11 @@
-require_relative 'auth.rb'
+require_relative 'helpers/auth.rb'
+require_relative 'helpers/phone-email.rb'
 
 class Admin < Sequel::Model(:admins)
   include AuthenticateModel
+  extend SearchByUsername
+
+  
   plugin :timestamps, :create=>:enrolled_on, :update=>:updated_at, :update_on_create=>true
   plugin :validation_helpers
   plugin :association_dependencies
@@ -10,12 +14,18 @@ class Admin < Sequel::Model(:admins)
   many_to_one :school
 
   def quicklink(prod=false)
+
     st_url = prod ? "https://www.joinstorytime.com" : ENV['STORYTIME_URL']
 
     st_url = st_url.sub(/^https?\:\/\//, '').sub(/^www./,'')
 
-    if email and signature and self.school and password_digest
-      "#{st_url}/signin?email=#{email}&digest=#{self.password_digest}&role=admin"
+    username = email
+    if email.nil? or email.empty?
+      username = phone
+    end
+
+    if username and signature and self.school and password_digest
+      "#{st_url}/signin?username=#{username}&digest=#{self.password_digest}&role=admin"
     else
       ''
     end
