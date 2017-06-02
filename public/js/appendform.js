bb9f5e3960f1ac61f3a171c3f854111f1344f9dc
jdmcpeek <david.mcpeek@yale.edu>
2017-06-02 12:12:26 -0700

    remove unneeded js files

--- public/js/appendform.js
+++
@@ -1,7 +0,0 @@
-// script to append the first modal's field values to the form
-$( document ).ready(function() {
-  console.log("Love does not begin and end the way we seem to think it does. Love is a battle, love is a war; love is a growing up.");
-});
-
-
-
