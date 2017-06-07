6fa185f3ff4fe4e9f357cf516ea2080a178ed08d
jdmcpeek <david.mcpeek@yale.edu>
2017-06-07 12:33:01 -0700

    dashboard_teacher, dashboard_admin

--- app/app.rb
+++ app/app.rb
@@ -80,9 +80,9 @@ class App < Sinatra::Base
   get '/' do
     case session[:role]
     when 'admin'
-      redirect to '/dashboard/admin_dashboard'
+      redirect to '/dashboard/dashboard_admin'
     when 'teacher'
-      redirect to '/dashboard/dashboard'
+      redirect to '/dashboard/dashboard_teacher'
     else
       erb :'homepage/index', locals: {mixpanel_homepage_key: ENV['MIXPANEL_HOMEPAGE']}
     end 
