9a96c547a5df39581b30bd1f1cc04e6818821641
David Liu <davidliuliuliu@gmail.com>
2017-06-07 13:15:11 -0700

    added get-app modal to normal mobile signup process

--- app/login_signup.rb
+++ app/login_signup.rb
@@ -223,7 +223,7 @@ class LoginSignup < Sinatra::Base
        [session[:first_name], session[:last_name], session[:username], session[:password]].include? ''
        redirect to '/'
     end
-    erb :'signup/index', locals: {mixpanel_homepage_key: ENV['MIXPANEL_HOMEPAGE']}
+    erb :'signup/index', locals: {mixpanel_homepage_key: ENV['MIXPANEL_HOMEPAGE'], subtitle_app: "See how parents use Storytime to get free books on their phone.", header_app: "Get the StoryTime app"}
   end
 
 
